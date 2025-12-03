c140d0e6a3a5b9a3bbdee8112ffc5f88d5605663
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-03 11:22:01 -0800

    day 3 both parts

---
+++ solutions/2025/day_03/solution.py
@@ -0,0 +1,52 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2025/day/3
+
+from ...base import StrSplitSolution, answer
+from collections import deque
+
+
+class Solution(StrSplitSolution):
+    _year = 2025
+    _day = 3
+
+    @answer(17408)
+    def part_1(self) -> int:
+        largestJoltage = 0
+        for bank in self.input:
+            tens = "0"
+            ones = bank[-1]
+            for i in range(len(bank) - 1):
+                if bank[i] > tens:
+                    tens = bank[i]
+                    ones = bank[-1]
+                elif bank[i] > ones:
+                    ones = bank[i]
+            largestJoltage += int(tens + ones)
+        return largestJoltage
+            
+
+    @answer(172740584266849)
+    def part_2(self) -> int:
+        largestJoltage = 0
+        for bank in self.input:
+            bestJoltage = []
+            q = deque()
+            for i in range(len(bank)):
+                if i >= len(bank) - 11 and q:
+                    bestJoltage.append(q.popleft())
+                while q and bank[i] > q[-1]:
+                    q.pop()
+                q.append(bank[i])
+            bestJoltage.append(q[0])
+            
+            print(("").join(bestJoltage))
+
+            largestJoltage += int("".join(bestJoltage))
+
+        return largestJoltage
+
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
