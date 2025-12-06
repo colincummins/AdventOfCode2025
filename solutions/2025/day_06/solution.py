0e9522ab04a1c23b26d613931190c41f7f691231
Colin Cummins <84826067+colincummins@users.noreply.github.com>
2025-12-05 23:39:04 -0800

    Day 6 Part 1 Complete

--- solutions/2025/day_06/solution.py
+++ solutions/2025/day_06/solution.py
@@ -3,15 +3,22 @@
 # puzzle prompt: https://adventofcode.com/2025/day/6
 
 from ...base import StrSplitSolution, answer
+from re import split
+from operator import add, mul
 
 
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 6
 
-    # @answer(1234)
+    @answer(4771265398012)
     def part_1(self) -> int:
-        pass
+        operators = [add if x == "+" else mul for x in self.input.pop().split()]
+        accumulator = map(int,self.input.pop().split())
+        while self.input:
+            current = map(int,self.input.pop().split())
+            accumulator = map(lambda a, b, opp: opp(a,b), accumulator, current, operators)
+        return sum(accumulator)
 
     # @answer(1234)
     def part_2(self) -> int:
