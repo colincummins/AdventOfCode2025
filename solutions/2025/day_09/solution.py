a6464f227df1516bd7a97c5b1428c9a8180abd6b
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-10 08:58:14 -0800

    Point now has multiply method to calculate rectangle area

--- solutions/2025/day_09/solution.py
+++ solutions/2025/day_09/solution.py
@@ -12,6 +12,10 @@ class Point():
     x: int
     y: int
 
+    def __mul__(self, other) -> int:
+        return (abs(self.x - other.x) + 1) * (abs(self.y - other.y) + 1)
+
+
 
 class Solution(StrSplitSolution):
     _year = 2025
@@ -19,52 +23,6 @@ class Solution(StrSplitSolution):
 
     def parseInput(self):
         self.input = [Point(*map(int, row.split(","))) for row in self.input]
-
-    def getRectangleArea(self, pt1: Point, pt2: Point) -> int:
-        return (abs(pt1.x - pt2.x) + 1) * (abs(pt1.y - pt2.y) + 1)
-
-    def createPointFromIndex(self, xIndex: int, yIndex: int) -> Point:
-        return Point(self.xCoords[xIndex], self.yCoords[yIndex])
-    
-    @cache
-    def recFindRectangles(self, upper: int, lower: int, left: int, right: int) -> int:
-        hash = (upper, lower, left, right)
-
-        if self.memo.get(hash) is not None:
-            print("already seen", hash)
-            return self.memo.get(hash)
-
-        if upper > lower or left > right:
-            self.memo[hash] = 0
-            return 0
-
-
-        upperLeft = self.createPointFromIndex(left, upper)
-        lowerRight = self.createPointFromIndex(right, lower)
-        opposite1 = set([upperLeft, lowerRight])
-        lowerLeft = self.createPointFromIndex(left, lower)
-        upperRight = self.createPointFromIndex(right, upper)
-        opposite2 = set([lowerLeft, upperRight])
-
-        if self.memo.values() and self.getRectangleArea(upperLeft, lowerRight) <= max(self.memo.values()):
-            self.memo[hash] = 0
-            return 0
-
-
-        if opposite1.issubset(self.allRedTiles) or opposite2.issubset(self.allRedTiles):
-            self.memo[hash] = self.getRectangleArea(upperLeft, lowerRight)
-            print(self.memo[hash])
-            return self.memo[hash]
-
-        bestRectangle = max([
-        self.recFindRectangles(upper + 1, lower, left, right),
-        self.recFindRectangles(upper, lower - 1, left, right),
-        self.recFindRectangles(upper, lower, left + 1, right),
-        self.recFindRectangles(upper, lower, left, right - 1)])
-
-        self.memo[hash] = bestRectangle
-        return bestRectangle
-
 
     # @answer(1234)
     def part_1(self) -> int:
@@ -76,23 +34,9 @@ class Solution(StrSplitSolution):
 
     # @answer((1234, 4567))
     def solve(self) -> tuple[int, int]:
-        self.memo = {}
-        self.maxRect = 0
         self.parseInput()
-        self.allRedTiles = set()
-        self.xCoords = set()
-        self.yCoords = set()
-        for coord in self.input:
-            self.allRedTiles.add(coord)
-            self.xCoords.add(coord.x)
-            self.yCoords.add(coord.y)
-        self.xCoords = sorted(self.xCoords)
-        self.yCoords = sorted(self.yCoords)
+        
 
-        return (self.recFindRectangles(0, len(self.yCoords) - 1, 0, len(self.xCoords) - 1), 0)
-
-
-            
 
             
 
