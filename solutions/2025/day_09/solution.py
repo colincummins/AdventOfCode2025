2fa683d3e4497e32894e113c97471edd74924650
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-12 14:38:21 -0800

    Naive solution using shapely libraries. Removed unused vector work in progress

--- solutions/2025/day_09/solution.py
+++ solutions/2025/day_09/solution.py
@@ -3,12 +3,10 @@
 # puzzle prompt: https://adventofcode.com/2025/day/9
 
 from ...base import StrSplitSolution, answer
-from collections import namedtuple
-from dataclasses import dataclass
-from functools import cache
-from math import sqrt, atan2
-from ...utils.vectors import Point 
 from itertools import combinations
+from shapely import Point, Polygon, MultiPoint, envelope
+
+
 
 
 
@@ -16,6 +14,12 @@ from itertools import combinations
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 9
+
+    @staticmethod
+    def getBoxVolume(box: Polygon)->int:
+        minx, miny, maxx, maxy = box.bounds
+        return (maxx - minx + 1) * (maxy - miny + 1)
+
 
     def parseInput(self):
         self.input = [Point(*map(int, row.split(","))) for row in self.input]
@@ -31,11 +35,19 @@ class Solution(StrSplitSolution):
     # @answer((1234, 4567))
     def solve(self) -> tuple[int, int]:
         part1 = 0
+        part2 = 0
         self.parseInput()
+        tilePattern = Polygon(self.input)
         for a, b in combinations(self.input, 2):
-            part1 = max(part1, a.getRectArea(b))
+            currBox = envelope(MultiPoint([a, b]))
+            currVolume = self.getBoxVolume(currBox)
+            if currVolume > part1:
+                part1 = currVolume
+            if tilePattern.contains(currBox) and currVolume > part2:
+                part2 = currVolume
         
-        return (part1,0)
+
+        return (int(part1), int(part2))
         
 
 
