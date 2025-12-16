ae61cd9366a3bd497519177290c28283827d1627
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-16 14:48:34 -0800

    Still passing test. Gradually eliminating wait conditions.

--- solutions/2022/day_19/solution.py
+++ solutions/2022/day_19/solution.py
@@ -62,7 +62,7 @@ class Solution(StrSplitSolution):
             # Wait and build geodebot
 
             # Build geodes
-            if (clayBot == 0) or (ore < maxOreCost and oreBot < maxOreCost) or (clay < maxClayCost and clayBot < maxClayCost) or (obsidianBot and obsidian < maxObsidianCost and obsidianBot < maxObsidianCost):
+            if (clay < maxClayCost and clayBot < maxClayCost) or (obsidianBot and obsidian < maxObsidianCost and obsidianBot < maxObsidianCost):
                 scenarios.append(tryBlueprint(oreBot, clayBot, obsidianBot, geodeBot, ore + oreBot, clay + clayBot, obsidian + obsidianBot, geodes + geodeBot, turns + 1))
 
             return max(scenarios)
