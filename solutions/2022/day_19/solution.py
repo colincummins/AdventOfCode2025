478a1bc67de7bca86d9f9ac34d89f70fc1177158
Colin Cummins <84826067+colincummins@users.noreply.github.com>
2025-12-16 17:56:02 -0800

    Smarter waiting (#4)
    * Changed for clarity - we build a list of scenario outcomes and return the max of it
    
    * In anticipation of the new waiting code, don't return results for turns over 25
    
    * Skeleton for waiting conditions
    
    * Claybot wait implemented
    
    * Implemented wait/build claybot
    
    * Still passing test. Gradually eliminating wait conditions.
    
    * Entered notes for future work - TODO - Implement wait/build for obsidianBot and geodeBot, as well as a wait for last turn, then generic waiting shouldn't be necessary.
    
    * Solves part 1 quickly
    
    * Solves Part 2 Test
    
    * 2022 19 Part 2 Solved - previously stopped building oreBots too early

--- solutions/2022/day_19/solution.py
+++ solutions/2022/day_19/solution.py
@@ -5,6 +5,7 @@
 from ...base import StrSplitSolution, answer
 from re import findall
 from functools import cache
+from math import ceil
 
 
 class Solution(StrSplitSolution):
@@ -31,54 +32,62 @@ class Solution(StrSplitSolution):
             if turns == TURN_LIMIT:
                 return geodes
 
+            if turns > TURN_LIMIT:
+                return 0
+
             if geodeRobotOreCost <= ore and geodeRobotObsidianCost <= obsidian:
                 return tryBlueprint(oreBot, clayBot, obsidianBot, geodeBot + 1, ore + oreBot - geodeRobotOreCost, clay + clayBot, obsidian + obsidianBot - geodeRobotObsidianCost, geodes + geodeBot, turns + 1)
 
 
-            buildOreBot = 0
-            buildClayBot = 0
-            buildObsidianBot = 0
-            wait = 0
+            scenarios = [0]
 
-            if ore >= oreRobotCost and oreBot <= max(obsidianRobotOreCost, geodeRobotOreCost):
-                buildOreBot = tryBlueprint(oreBot + 1, clayBot, obsidianBot, geodeBot, ore + oreBot - oreRobotCost, clay + clayBot, obsidian + obsidianBot, geodes + geodeBot, turns + 1)
 
-            if ore >= clayRobotCost and clayBot <= obsidianRobotClayCost:
-                buildClayBot = tryBlueprint(oreBot, clayBot + 1, obsidianBot, geodeBot, ore + oreBot - clayRobotCost, clay + clayBot, obsidian + obsidianBot, geodes + geodeBot, turns + 1)
 
-            if ore >= obsidianRobotOreCost and clay >= obsidianRobotClayCost and obsidianBot <= geodeRobotObsidianCost:
-                buildObsidianBot = tryBlueprint(oreBot, clayBot, obsidianBot + 1, geodeBot, ore + oreBot - obsidianRobotOreCost, clay + clayBot - obsidianRobotClayCost, obsidian + obsidianBot, geodes + geodeBot, turns + 1)
+            # Wait and build orebot
+            if oreBot < max(obsidianRobotOreCost, geodeRobotOreCost, clayRobotCost):
+                turnsNeeded = max(0,ceil((oreRobotCost - ore) / oreBot)) + 1
+                scenarios.append(tryBlueprint(oreBot + 1, clayBot, obsidianBot, geodeBot, ore + (turnsNeeded * oreBot) - oreRobotCost, clay + turnsNeeded * clayBot, obsidian + turnsNeeded * obsidianBot, geodes + geodeBot * turnsNeeded, turns + turnsNeeded))
 
-            if (clayBot == 0) or (ore < maxOreCost and oreBot < maxOreCost) or (clay < maxClayCost and clayBot < maxClayCost) or (obsidianBot and obsidian < maxObsidianCost and obsidianBot < maxObsidianCost):
-                wait = tryBlueprint(oreBot, clayBot, obsidianBot, geodeBot, ore + oreBot, clay + clayBot, obsidian + obsidianBot, geodes + geodeBot, turns + 1)
+            # Wait and build claybot
+            if oreBot and clayBot < obsidianRobotClayCost:
+                turnsNeeded = max(0, ceil((clayRobotCost - ore) / oreBot)) + 1
+                scenarios.append(tryBlueprint(oreBot, clayBot + 1, obsidianBot, geodeBot, ore + (turnsNeeded * oreBot)- clayRobotCost, clay + turnsNeeded * clayBot, obsidian + turnsNeeded * obsidianBot, geodes + geodeBot * turnsNeeded, turns + turnsNeeded))
 
-            return max(wait, buildOreBot, buildClayBot, buildObsidianBot)
+
+            # Wait and build obsidianbot
+            if oreBot and clayBot and obsidianBot < geodeRobotObsidianCost:
+                turnsNeeded = max(0, ceil((obsidianRobotOreCost - ore) / oreBot), ceil((obsidianRobotClayCost - clay)/clayBot)) + 1
+                scenarios.append(tryBlueprint(oreBot, clayBot, obsidianBot + 1, geodeBot, ore + (turnsNeeded * oreBot)- obsidianRobotOreCost, clay + (turnsNeeded * clayBot) - obsidianRobotClayCost, obsidian + turnsNeeded * obsidianBot, geodes + geodeBot * turnsNeeded, turns + turnsNeeded))
+
+            # Wait and build geodebot
+            if oreBot and obsidianBot:
+                turnsNeeded = max(0, ceil((geodeRobotOreCost - ore) / oreBot), ceil((geodeRobotObsidianCost - obsidian)/obsidianBot)) + 1
+                scenarios.append(tryBlueprint(oreBot, clayBot, obsidianBot, geodeBot + 1, ore + (turnsNeeded * oreBot)- geodeRobotOreCost, clay + turnsNeeded * clayBot, obsidian + turnsNeeded * obsidianBot - geodeRobotObsidianCost, geodes + geodeBot * turnsNeeded, turns + turnsNeeded))
+
+            # Wait until last turn
+            if geodeBot:
+                turnsNeeded = TURN_LIMIT - turns
+                scenarios.append(tryBlueprint(oreBot, clayBot, obsidianBot, geodeBot, ore + (turnsNeeded * oreBot), clay + turnsNeeded * clayBot, obsidian + turnsNeeded * obsidianBot, geodes + geodeBot * turnsNeeded, turns + turnsNeeded))
+
+
+            return max(scenarios)
             
         part1 = 0
         TURN_LIMIT = 25
         for line in self.input:
             tryBlueprint.cache_clear()
             blueprintNum, oreRobotCost, clayRobotCost, obsidianRobotOreCost, obsidianRobotClayCost, geodeRobotOreCost, geodeRobotObsidianCost = self.extractNums(line)
-            maxOreCost = max(oreRobotCost, clayRobotCost, obsidianRobotOreCost, geodeRobotOreCost)
-            maxClayCost = obsidianRobotClayCost
-            maxObsidianCost = geodeRobotObsidianCost
-
             buildResult = tryBlueprint(1, 0, 0, 0, 0, 0, 0, 0, 1) 
             print("Blueprint: ", blueprintNum, " resulted in", buildResult, " geodes")
             part1 += buildResult * blueprintNum
 
         part2 = 1
-        """
-        TURN_LIMIT = 32
+        TURN_LIMIT = 33
         for line in self.input[:3]:
             tryBlueprint.cache_clear()
             blueprintNum, oreRobotCost, clayRobotCost, obsidianRobotOreCost, obsidianRobotClayCost, geodeRobotOreCost, geodeRobotObsidianCost = self.extractNums(line)
-            maxOreCost = max(oreRobotCost, clayRobotCost, obsidianRobotOreCost, geodeRobotOreCost)
-            maxClayCost = obsidianRobotClayCost
-            maxObsidianCost = geodeRobotObsidianCost
-
             buildResult = tryBlueprint(1, 0, 0, 0, 0, 0, 0, 0, 1) 
             print("Blueprint: ", blueprintNum, " resulted in", buildResult, " geodes")
             part2 *= buildResult
-            """
+
         return part1, part2
