2b6b29cf196adb2b3bacfc5c77944e9701ad2102
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-18 10:37:05 -0800

    2022 Day 21 Initial Commit

---
+++ solutions/2022/day_21/solution.py
@@ -0,0 +1,22 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2022/day/21
+
+from ...base import StrSplitSolution, answer
+
+
+class Solution(StrSplitSolution):
+    _year = 2022
+    _day = 21
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
