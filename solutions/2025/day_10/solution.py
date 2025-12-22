d742adab0af80f899fc59ca72d0c0f65f0316d84
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-22 11:56:41 -0800

    Start reworking 2025-10-2. Change input parsing so part1 and part2 parse line by line without affecting self.input itself.

--- solutions/2025/day_10/solution.py
+++ solutions/2025/day_10/solution.py
@@ -6,6 +6,7 @@ from ...base import StrSplitSolution, answer
 from collections import deque, defaultdict
 from heapq import heappush, heappop
 from math import inf
+from functools import cache
 
 
 class Solution(StrSplitSolution):
@@ -15,7 +16,7 @@ class Solution(StrSplitSolution):
     def printReg(self, register):
         return "".join(map(lambda x: "." if x == "0" else "#", reversed(bin(register)[2:])))
 
-    def parseLine(self, line):
+    def parseLine1(self, line):
         elements = line.split(" ")
         lights = int("".join(map(lambda x: "1" if x=="#" else "0", elements[0].strip("[]")[::-1])),2)
         buttons = []
@@ -28,8 +29,6 @@ class Solution(StrSplitSolution):
 
         return lights, buttons, joltages
 
-    def parseInput(self):
-        self.input = [self.parseLine(line) for line in self.input]
 
     def getAstar(self, joltages: tuple[int]) -> int:
         return (sum(map(lambda x: x**2, joltages)))**(1/len(joltages))
@@ -45,72 +44,51 @@ class Solution(StrSplitSolution):
     def part_2(self) -> int:
         pass
 
-    # @answer((1234, 4567))
+    @answer((401, 0))
     def solve(self) -> tuple[int, int]:
         part1 = 0
-        self.parseInput()
-        for lights, buttons, joltages in self.input:
+        for line in self.input:
+            lights, buttons, joltages = self.parseLine1(line)
             visited = set()
             q = deque([(0, 0)])
             while q:
-                self.debug(q)
                 steps, curr = q.popleft()
                 if curr in visited:
                     continue
                 visited.add(curr)
                 if curr == lights:
-                    self.debug(steps)
                     part1 += steps
                     break
                 for button in buttons:
                     q.append((steps + 1, button ^ curr))
 
         #part 2
+
         part2 = 0
+        for line in self.input:
+            self.debug(line)
+        """
+        @cache
+        def recNumPresses(remainingJoltage: tuple[int]) -> int:
+            if any([x < 0 for x in remainingJoltage]):
+                return inf
+            if all([x == 0 for x in remainingJoltage]):
+                return 0
+
+
+        assert recNumPresses((0,0,0,0)) == 0
+        assert recNumPresses((-1,0,0,0)) == inf
         for _, buttons, joltages in self.input:
+            recNumPresses.cache_clear()
             self.debug("New Line:")
-            visited = set()
-
-            dist = defaultdict(lambda: inf)
-            astar = defaultdict(lambda: inf)
-
-            dist[joltages] = 0
-            astar[joltages] = self.getAstar(joltages)
-
-            # AStar Distance, Current Distance, Joltages
-            heap = [(0, 0, tuple(joltages))]
-            visited = set()
-
-            while heap:
-                self.debug("Lights:", lights, "Buttons:", buttons, "Joltages:", *joltages)
-                currAStar, currDist, joltages = heappop(heap)
-                visited.add(joltages)
-                if all(x == 0 for x in joltages):
-                    part2 += currDist
-                    break
-                for button in buttons:
-                    neighbor = list(joltages)
-                    digit = 0
-                    while button != 0:
-                        self.debug("Button:", button)
-                        if button & 1:
-                            neighbor[digit] -= 1
-                        button >>= 1 
-                        digit += 1
-                    if any(x < 0 for x in neighbor):
-                        continue
-                    neighbor = tuple(neighbor)
-                    if dist[joltages] + 1 < dist[neighbor]:
-                        dist[neighbor] = dist[joltages] + 1
-                        astar[neighbor] = dist[joltages] + 1 + self.getAstar(neighbor)
-                        if neighbor not in visited:
-                            heappush(heap, (astar[neighbor], dist[neighbor], neighbor))
+            self.debug("Buttons{}".format(buttons))
 
 
 
 
 
             # Remember - no negative joltages allowed
+        """
 
 
         return (part1, part2)
