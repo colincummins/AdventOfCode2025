a1775a99adce1f0701b64af1dd839d604f42ffbc
Colin Cummins <all.our.august.sundays@gmail.com>
2025-12-26 13:05:37 -0800

    Passes part 1

--- solutions/2025/day_11/solution.py
+++ solutions/2025/day_11/solution.py
@@ -3,15 +3,41 @@
 # puzzle prompt: https://adventofcode.com/2025/day/11
 
 from ...base import StrSplitSolution, answer
+from collections import defaultdict
 
 
 class Solution(StrSplitSolution):
     _year = 2025
     _day = 11
 
-    # @answer(1234)
+    @answer(470)
     def part_1(self) -> int:
-        pass
+        downstreamDict = {}
+        visited = set()
+        self.paths = 0
+        for line in self.input:
+            node, downstreamNodes = line.split(": ")
+            downstreamDict[node] = list(downstreamNodes.split(" "))
+
+        def aux(node: str) -> None:
+            if node in visited:
+                return
+
+            visited.add(node)
+
+            if node == "out":
+                self.paths += 1
+
+            else:
+                for downstream in downstreamDict[node]:
+                    aux(downstream)
+
+            visited.remove(node)
+
+        aux("you")
+        return self.paths
+
+
 
     # @answer(1234)
     def part_2(self) -> int:
