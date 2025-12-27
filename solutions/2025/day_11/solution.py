7da940efd43db6c5760958b4e73139701e613082
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2025-12-26 18:24:57 -0800

    Still working on upstream traversal

--- solutions/2025/day_11/solution.py
+++ solutions/2025/day_11/solution.py
@@ -73,7 +73,7 @@ class Solution(StrSplitSolution):
             visited.remove(node)
 
 
-        aux("fft", "dac", downstreamDict) 
+        aux("out", "dac", upstreamDict, "fft") 
 
 
         """
