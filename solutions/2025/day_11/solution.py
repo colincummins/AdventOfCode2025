b2200352ed0404c7da41ec60d7bcde038f6aad58
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2025-12-26 18:34:44 -0800

    attempting downstream traversal

--- solutions/2025/day_11/solution.py
+++ solutions/2025/day_11/solution.py
@@ -73,7 +73,7 @@ class Solution(StrSplitSolution):
             visited.remove(node)
 
 
-        aux("dac", "out", downstreamDict, "out") 
+        aux("svr", "fft", downstreamDict, "dac") 
 
 
         """
